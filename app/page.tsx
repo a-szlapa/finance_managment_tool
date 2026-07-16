5e3ca7e373ed90b5695254c852b94b024d6d612d
AleksySzlapa <szlapaaleksy@gmail.com>
2026-07-16 07:05:05 +0200

    refactor: apply new file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import React, { useState } from "react"
 
 function page() {
+  const [showModal, setShowModal] = useState(false)
   return (
-    <div>page</div>
+    <>
+      
+    </>
   )
 }
 
