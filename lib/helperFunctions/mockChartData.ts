4cb8e6bbaa6d98016095569abac6f68f87db5185
AleksySzlapa <szlapaaleksy@gmail.com>
2026-07-24 03:38:05 +0200

    fix: mock data no longer repeats the same day

--- lib/helperFunctions/mockChartData.ts
+++ lib/helperFunctions/mockChartData.ts
@@ -1,7 +1,6 @@
 import { chartData, chartDataPoint } from "../types/chatData"
 
 function genChartDataPoint(date: Date): chartDataPoint {
-
   let temp = Number((Math.random() * (1000 - 800) + 800).toFixed(2))
 
   return {
@@ -13,11 +12,9 @@ function genChartDataPoint(date: Date): chartDataPoint {
 }
 
 export default function genChartData(length: number): chartData {
-  let output:chartData = []
-  Array.from({length: length}).forEach(i => {
-    const date = new Date();
-    date.setDate(date.getDate() + 3)
-    output.push(genChartDataPoint(date))
-  });
-  return output
+  const startDate = new Date()
+
+  return Array.from({ length }, (_, i) =>
+    genChartDataPoint(new Date(startDate.setDate(startDate.getDate() + i)))
+  )
 }
